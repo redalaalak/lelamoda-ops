98eb80ef56ff0fd45327660dd9b1d1c4848739d3
redalaalak <redalaalak@gmail.com>
2026-04-19 09:47:15 +0100

    fix: add useEffect import to EditOrderPanel (build error)

--- components/orders/EditOrderPanel.tsx
+++ components/orders/EditOrderPanel.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useRef, useCallback } from 'react'
+import { useState, useRef, useCallback, useEffect } from 'react'
 import Link from 'next/link'
 
 // ---------------------------------------------------------------------------
